8c5b7d5852f7462968f867819ddb5cd05a97978d
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-07-31 23:18:27 -0500

    add

--- gradle_spring_1/src/main/java/com/example/demo/ApankuraController.java
+++ gradle_spring_1/src/main/java/com/example/demo/ApankuraController.java
@@ -1,15 +1,14 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static java.net.NetworkInterface.getNetworkInterfaces;
 
 @RestController()
 @RequestMapping("/apankura")
@@ -17,9 +16,9 @@ public class ApankuraController {
 
     @RequestMapping("/info")
     public Map<String, String> Information() throws SocketException {
-        HashMap<String, String> map = new HashMap<>();
-
-        Enumeration e = NetworkInterface.getNetworkInterfaces();
+        HashMap<String, String> map = new HashMap<String,String>();
+        Enumeration e = getNetworkInterfaces();
+        StringBuilder builder = new StringBuilder();
         while(e.hasMoreElements())
         {
             NetworkInterface n = (NetworkInterface) e.nextElement();
@@ -27,10 +26,10 @@ public class ApankuraController {
             while (ee.hasMoreElements())
             {
                 InetAddress i = (InetAddress) ee.nextElement();
-                System.out.println(i.getHostAddress());
+                builder.append(i.getHostAddress()).append(";");
             }
         }
-
+        map.put( "netowrk", builder.toString());
         return map;
     }
 
@@ -40,6 +39,66 @@ public class ApankuraController {
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         HashMap<String, String> map = new HashMap<>();
         map.put("date", formatter.format(date));
+        return map;
+    }
+
+    @RequestMapping("/session/all")
+    public Map<String, String> AllSession(HttpSession session) {
+        HashMap<String, String> map = new HashMap<>();
+        for (String key: Collections.list( session.getAttributeNames() ) ){
+            map.put(key, (String)session.getAttribute(key));
+        }
+        return map;
+    }
+
+    @RequestMapping("/session")
+    public Map<String, String> GetSession(HttpSession session,
+                                          @RequestParam String key) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(key, (String)session.getAttribute(key));
+        return map;
+    }
+
+    @PostMapping("/session")
+    public Map<String, String> PostSession(HttpSession session,
+                                           @RequestParam String key,
+                                           @RequestParam String data) {
+        session.setAttribute(key, data);
+        HashMap<String, String> map = new HashMap<>();
+        map.put(key, data);
+        return map;
+    }
+
+    @PutMapping("/session")
+    public Map<String, String> PutSession(HttpSession session,
+                                           @RequestParam String key,
+                                           @RequestParam String data) {
+        session.setAttribute(key, data);
+        HashMap<String, String> map = new HashMap<>();
+        map.put(key, data);
+        return map;
+    }
+
+    @DeleteMapping("/session")
+    public Map<String, String> DeleteSession(HttpSession session,
+                                          @RequestParam String key) {
+        session.removeAttribute(key);
+        HashMap<String, String> map = new HashMap<>();
+        return map;
+    }
+
+    @DeleteMapping("/session/all")
+    public Map<String, String> DeleteAllSession(HttpSession session) {
+        for (String key: Collections.list( session.getAttributeNames() ) ){
+            session.removeAttribute(key);
+        }
+        HashMap<String, String> map = new HashMap<>();
+        return map;
+    }
+    @DeleteMapping("/session/invalidate")
+    public Map<String, String> Invalidate(HttpSession session) {
+        session.invalidate();
+        HashMap<String, String> map = new HashMap<>();
         return map;
     }
 }
