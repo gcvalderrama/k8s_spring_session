e22e893c3efb7edc6c28dadf02e3c8a5f80dc5ce
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-04-09 23:05:08 -0500

    add

--- gradle_spring_1/containers/build.ps1
+++ gradle_spring_1/containers/build.ps1
@@ -1,3 +1,3 @@
 docker-compose build
-docker tag sut/spring-session localhost:5000/sut/spring-session
-docker push localhost:5000/sut/spring-session
+docker tag sut/spring-session localhost:32000/sut/spring-session
+docker push localhost:32000/sut/spring-session
